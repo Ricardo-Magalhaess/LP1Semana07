8107004fbd9e1a302d9e6c117ce454d872fddc47
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-04-19 14:36:19 +0100

    Foi modificado a classe Program

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,7 +6,27 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool t = true;
+            Unit[] units = new Unit[]
+            {
+                new MilitaryUnit(2, 7, 1),
+                new MilitaryUnit(1, 3, 2),
+                new MilitaryUnit(13, 4, 23),
+                new SettlerUnit(),
+                
+            };
+
+            
+
+            (units[0] as MilitaryUnit).Attack(units[1]);
+            (units[1] as MilitaryUnit).Attack(units[3]);
+            (units[2] as MilitaryUnit).Attack(units[2]);
+
+            foreach (Unit u in units)
+            {                
+                
+                Console.WriteLine($" {u} movement- {u.Move()}");
+            }
         }
     }
 }
